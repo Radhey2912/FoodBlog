eb3f0d3c9db56a259120337a9b200fd7e9ad7268
Radhey <Radhey Mugdal>
2022-09-04 17:21:00 +0530

    adding script for heruko

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const fs=require('fs')
 const path=require('path');
 const app =express();
-const port=80;
+const port=process.env.PORT;
 const bodyparser=require('body-parser');
 const mongoose = require('mongoose');
 
